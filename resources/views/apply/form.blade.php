bb2f0d9e5a14984b4676087009db36678db0a12b
Chris Rowles <chris.rowles@godeploy.it>
2020-11-25 21:05:35 +0000

    Sorted

--- resources/views/apply/form.blade.php
+++ resources/views/apply/form.blade.php
@@ -12,7 +12,7 @@
                 </div>
             </div>
         </div>
-        <div class="container">
+        <div class="container pb-4">
             <div class="row">
                 <div class="col-md-12">
                     <div class="card bg-dark filter">

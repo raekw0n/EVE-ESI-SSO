b3fcd2f1984c5dade6db4e15c9b5145f21ee0900
Chris Rowles <chris.rowles@godeploy.it>
2020-11-27 17:54:28 +0000

    Make homepage more responsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,8 +16,8 @@
                             <p class="font-big">
                                 We're a logistics corporation operating throughout New Eden.
                             </p>
-                            <div class="row d-none d-md-flex">
-                                <div class="col-2">
+                            <div class="row">
+                                <div class="col-12 col-sm-6 col-md-4 col-lg-2">
                                     <div class="card filter" style="height: initial;">
                                         <div class="card-body">
                                             <h2 class="text-success">{{ $contracts }}</h2>
@@ -25,7 +25,7 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-2">
+                                <div class="col-12 col-sm-6 col-md-4 col-lg-2 mt-3 mt-sm-0">
                                     <div class="card filter" style="height: initial;">
                                         <div class="card-body">
                                             <h2 class="text-success">9</h2>
@@ -34,8 +34,8 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="row py-4 d-none d-md-flex">
-                                <div class="col-4">
+                            <div class="row py-4">
+                                <div class="col-12 col-md-8">
                                     <div class="card filter">
                                         <div class="card-body">
                                         @include('partials.calculators.haulage')
